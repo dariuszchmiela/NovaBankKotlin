f4970d8a451a53964a8f95612145b0e8bdd8c8ab
Dariusz Chmiela <dariusz.chmiela@o2.pl>
2026-09-19 22:42:58 +0200

    Initial commit

---
+++ src/test/kotlin/pl/dch/novabank/NovaBankKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.dch.novabank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NovaBankKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
