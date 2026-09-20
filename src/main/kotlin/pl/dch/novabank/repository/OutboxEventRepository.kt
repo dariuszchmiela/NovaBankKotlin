cc9f55a584fa6a7863936d40240b9a41c0ceadda
Dariusz Chmiela <dariusz.chmiela@o2.pl>
2026-09-20 10:06:48 +0200

    Add OutboxEvent repository

---
+++ src/main/kotlin/pl/dch/novabank/repository/OutboxEventRepository.kt
@@ -0,0 +1,10 @@
+﻿package pl.dch.novabank.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import pl.dch.novabank.entity.OutboxEvent
+import java.util.UUID
+
+interface OutboxEventRepository : JpaRepository<OutboxEvent, UUID> {
+
+    fun findByPublishedAtIsNullOrderByCreatedAtAsc(): List<OutboxEvent>
+}
